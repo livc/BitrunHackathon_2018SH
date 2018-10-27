47bd3a8c1ba50dd0ff52c0cebbe49272b21980b7
“5sWind” <fiveswind@gmail.com>
2018-10-27 19:26:31 +0800

    milestone.

--- contracts/AderDai.sol
+++ contracts/AderDai.sol
@@ -8,24 +8,10 @@ pragma solidity ^0.4.25;
 //     _    _  _ _|_ _  .
 //    (/_\/(/_| | | _\  .
 //==============================================================================
-library AderDaidatasets {
-    struct Bidder {
-        uint256 id;   // bidder id
-        uint256 gen;    // general vault
-        bool winner;   // bidder winner
-    }
-    struct EventReturns {
-        uint256 compressedData;
-        uint256 compressedIDs;
-        address winnerAddr;         // winner address
-        uint256 genAmount;          // amount distributed to gen
-    }
-}
+import "./DSToken.sol";
 
-
-
-contract AderDai {
-    address constant private Dai = "0xc4375b7de8af5a38a93548eb8453a498222c4ff2";
+contract AderDai is DSToken {
+    address constant private testAccount = 0x3fa17c1f1a0ae2db269f0b572ca44b15bc83929a;
 //==============================================================================
 //     _ _  _  |`. _     _ _ |_ | _  _  .
 //    (_(_)| |~|~|(_||_|| (_||_)|(/__\  .  (game settings)
@@ -38,8 +24,12 @@ contract AderDai {
 // BIDDER DATA 
 //****************
     mapping (address => uint256) public bIDxAddr_;          // (addr => bID) returns bidder id by address
-    mapping (address => AderDaidatasets.Bidder) public bidder_; // (addr => data) returns bidder info by address
+    mapping (uint256 => AderDaidatasets.Bidder) public bidder_; // (bID => data) returns bidder info by bidder id
+//****************
+// GAME DATA 
+//****************
     uint256 public bidCount_;
+    uint256 public bidPrice_;
 
 //==============================================================================
 //     _ _  _  __|_ _    __|_ _  _  .
@@ -49,6 +39,7 @@ contract AderDai {
         public
     {
         bidCount_ = 0;
+        bidPrice_ = initPrice_;
     }
 
 //==============================================================================
@@ -66,6 +57,14 @@ contract AderDai {
         _;
     }
 
+    /**
+     * @dev sets the price to limited price
+     */
+    modifier priceLimited() {
+        require(msg.value == bidPrice_, "sorry limited price");
+        _;
+    }
+
 //==============================================================================
 //     _    |_ |. _   |`    _  __|_. _  _  _  .
 //    |_)|_||_)||(_  ~|~|_|| |(_ | |(_)| |_\  .  (use these to interact with contract)
@@ -75,6 +74,7 @@ contract AderDai {
      */
     function()
         isHuman()
+        priceLimited()
         public
         payable
     {
@@ -88,20 +88,43 @@ contract AderDai {
         buyCore(_bID, _eventData_);
     }
 
+    /**
+     * @dev logic runs whenever a buy order is executed. 
+     */
     function buyCore(uint256 _bID, AderDaidatasets.EventReturns memory _eventData_)
         private
     {
-        // if bidding is active
-        if (bidder_[msg.sender].winner != true)
-        {
-
+        // transfer Dai to testAccount
+        transfer(testAccount, msg.value);
+        // update bidding price
+        bidPrice_ = calcBidPrice();
+        bidder_[_bID].owner = true;
+        // if bidder more than 1, set bID-1 to false
+        if (_bID > 1) {
+            bidder_[_bID-1].owner = false;
         }
-        // if bidding is not active
-        else 
-        {
+    }
 
-        }
+    /**
+     * @dev request badding price.
+     */
+    function getBidPrice()
+        public
+        view
+        returns(uint256)
+    {
+        return (bidPrice_);
+    }
 
+    /**
+     * @dev calculate badding price.
+     */
+    function calcBidPrice()
+        private
+        view
+        returns(uint256)
+    {
+        return (bidPrice_.mul(bidRate_));
     }
 
     /**
@@ -114,10 +137,150 @@ contract AderDai {
     {
         bidCount_++;
         bIDxAddr_[msg.sender] = bidCount_;
-        bidder_[msg.sender].id = bidCount_;
+        bidder_[bidCount_].id = bidCount_;
+        bidder_[bidCount_].addr = msg.sender;
         
         // set the new player bool to true
         _eventData_.compressedData = _eventData_.compressedData + 1;
         return (_eventData_);
+    }
+}
+
+/**
+* @title AderDai datasets
+*/
+library AderDaidatasets {
+    struct Bidder {
+        uint256 id;   // bidder id
+        address addr; // bidder address
+        uint256 gen;    // general vault
+        bool owner;   // ad owner
+    }
+    struct EventReturns {
+        uint256 compressedData;
+        uint256 compressedIDs;
+        address winnerAddr;         // winner address
+        uint256 genAmount;          // amount distributed to gen
+    }
+}
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ * change notes:  original SafeMath library from OpenZeppelin modified by Inventor
+ * - added sqrt
+ * - added sq
+ * - added pwr 
+ * - changed asserts to requires with error log outputs
+ */
+library SafeMath {
+    
+    /**
+    * @dev Multiplies two numbers, throws on overflow.
+    */
+    function mul(uint256 a, uint256 b) 
+        internal 
+        pure 
+        returns (uint256 c) 
+    {
+        if (a == 0) {
+            return 0;
+        }
+        c = a * b;
+        require(c / a == b, "SafeMath mul failed");
+        return c;
+    }
+
+    /**
+    * @dev Integer division of two numbers, truncating the quotient.
+    */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0); // Solidity only automatically asserts when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+    
+    /**
+    * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    */
+    function sub(uint256 a, uint256 b)
+        internal
+        pure
+        returns (uint256) 
+    {
+        require(b <= a, "SafeMath sub failed");
+        return a - b;
+    }
+
+    /**
+    * @dev Adds two numbers, throws on overflow.
+    */
+    function add(uint256 a, uint256 b)
+        internal
+        pure
+        returns (uint256 c) 
+    {
+        c = a + b;
+        require(c >= a, "SafeMath add failed");
+        return c;
+    }
+    
+    /**
+     * @dev gives square root of given x.
+     */
+    function sqrt(uint256 x)
+        internal
+        pure
+        returns (uint256 y) 
+    {
+        uint256 z = ((add(x,1)) / 2);
+        y = x;
+        while (z < y) 
+        {
+            y = z;
+            z = ((add((x / z),z)) / 2);
+        }
+    }
+    
+    /**
+     * @dev gives square. multiplies x by x
+     */
+    function sq(uint256 x)
+        internal
+        pure
+        returns (uint256)
+    {
+        return (mul(x,x));
+    }
+    
+    /**
+     * @dev x to the power of y 
+     */
+    function pwr(uint256 x, uint256 y)
+        internal 
+        pure 
+        returns (uint256)
+    {
+        if (x==0)
+            return (0);
+        else if (y==0)
+            return (1);
+        else 
+        {
+            uint256 z = x;
+            for (uint256 i=1; i < y; i++)
+                z = mul(z,x);
+            return (z);
+        }
+    }
+
+    /**
+    * @dev Divides two numbers and returns the remainder (unsigned integer modulo),
+    * reverts when dividing by zero.
+    */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b != 0);
+        return a % b;
     }
 }
