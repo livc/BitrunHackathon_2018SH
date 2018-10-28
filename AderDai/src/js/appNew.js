f895b848e88d4eef270f8cbe9bf7fa89371f23bd
Juncheng Zhu <zhujuncheng1021@163.com>
2018-10-28 09:12:08 +0800

    feature BID

--- AderDai/src/js/appNew.js
+++ AderDai/src/js/appNew.js
@@ -4,15 +4,15 @@ App = {
 
   init: function() {
     // Load pets.
-    $.getJSON('../bid.json', function(data) {
+    $.getJSON('../pets.json', function(data) {
       var petsRow = $('#petsRow');
       var petTemplate = $('#petTemplate');
 
       for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].bidder);
+        petTemplate.find('.panel-title').text(data[i].name);
         petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].bidValue);
-        petTemplate.find('.pet-age').text(data[i].date);
+        petTemplate.find('.pet-breed').text(data[i].breed);
+        petTemplate.find('.pet-age').text(data[i].age);
         petTemplate.find('.pet-location').text(data[i].location);
         petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
 
